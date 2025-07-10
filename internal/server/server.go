8047aeda949cd49ca9281f8bef6568847bd73a73
Svyatoslav Shuvaev <fr11ck11sl@gmail.com>
2025-07-10 16:42:06 +0300

    first commit

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,40 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	logger     *log.Logger
+	httpServer *http.Server
+}
+
+func CreateServer(logger *log.Logger) *Server {
+	r := chi.NewRouter()
+
+	r.Get("/", handlers.ReturnData)
+	r.Post("/upload", handlers.ReturnConverted)
+
+	myServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		logger:     logger,
+		httpServer: myServer,
+	}
+}
+func (s *Server) Start() error {
+	s.logger.Println("Сервер запускается на", s.httpServer.Addr)
+	return s.httpServer.ListenAndServe()
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,14 @@
 package service
+
+import (
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(s string) string {
+	for _, r := range s {
+		if r != '.' && r != '-' && r != ' ' {
+			return morse.ToText(s)
+		}
+	}
+	return morse.ToMorse(s)
+}
